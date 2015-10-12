a36a201947698419f176d7776c89b4e5991aa892
xortiz <zavierazo@gmail.com>
2015-10-12 12:10:33 +0200

    Add MaterialDesign menu.

--- app/mobile/src/main/java/wearable/hotelbeds/traveler/MainActivity.java
+++ app/mobile/src/main/java/wearable/hotelbeds/traveler/MainActivity.java
@@ -1,27 +1,39 @@
 package wearable.hotelbeds.traveler;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-
-import wearable.hotelbeds.shared.event.EventInfoBean;
 import wearable.hotelbeds.shared.event.EventUtils;
+import wearable.hotelbeds.traveler.nav.FragmentDrawer;
+import wearable.hotelbeds.traveler.nav.MenuUtils;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private static String LOG_TAG = "RecyclerViewActivity";
+    private Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
+    private MenuItem mSearchAction;
+    private boolean isSearchOpened = false;
+    private EditText edtSeach;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +49,14 @@ public class MainActivity extends Activity {
         RecyclerView.ItemDecoration itemDecoration =
                 new DividerItemDecorator(this, LinearLayoutManager.VERTICAL);
         mRecyclerView.addItemDecoration(itemDecoration);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerFragment = (FragmentDrawer) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
 
         // Code to Add an item with default animation
         //((MyRecyclerViewAdapter) mAdapter).addItem(obj, index);
@@ -56,5 +76,91 @@ public class MainActivity extends Activity {
                                                                                  }
                                                                              });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        mSearchAction = menu.findItem(R.id.action_search);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_search) {
+            handleMenuSearch();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+        MenuUtils.onMenuSelected(this, position);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isSearchOpened) {
+            handleMenuSearch();
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    private void doSearch(String text) {
+        Log.i(LOG_TAG, "Buscando..."+text);
+        handleMenuSearch();//Hide search
+    }
+    private void handleMenuSearch() {
+        ActionBar action = getSupportActionBar(); //get the actionbar
+
+        if (isSearchOpened) { //test if the search is open
+            action.setDisplayShowCustomEnabled(false); //disable a custom view inside the actionbar
+            action.setDisplayShowTitleEnabled(true); //show the title in the action bar
+
+            //hides the keyboard
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(edtSeach.getWindowToken(), 0);
+
+            //add the search icon in the action bar
+            mSearchAction.setIcon(R.drawable.ic_action_search);
+            //Hide keyboard
+            InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+            isSearchOpened = false;
+        } else { //open the search entry
+
+            action.setDisplayShowCustomEnabled(true); //enable it to display a
+            // custom view in the action bar.
+            action.setCustomView(R.layout.search_bar);//add the custom view
+            action.setDisplayShowTitleEnabled(false); //hide the title
+
+            edtSeach = (EditText) action.getCustomView().findViewById(R.id.edtSearch); //the text editor
+
+            //this is a listener to do a search when the user clicks on search button
+            edtSeach.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                        doSearch(v.getText().toString());
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            edtSeach.requestFocus();
+            //open the keyboard focused in the edtSearch
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(edtSeach, InputMethodManager.SHOW_IMPLICIT);
+
+            //add the close icon
+            mSearchAction.setIcon(R.drawable.ic_close_black_24dp);
+
+            isSearchOpened = true;
+        }
+    }
+
+
 }
 
---
+++ app/mobile/src/main/java/wearable/hotelbeds/traveler/nav/MenuUtils.java
@@ -0,0 +1,29 @@
+package wearable.hotelbeds.traveler.nav;
+
+import android.content.Context;
+
+/**
+ * Created by Zavierazo on 12/10/2015.
+ */
+public class MenuUtils {
+
+    public static void onMenuSelected(Context context, int position) {
+        //TODO Make actions
+        switch (position) {
+            case 0:
+                //Profile
+
+                break;
+            case 1:
+                //Home
+
+                break;
+            case 2:
+                //Bookings
+
+                break;
+            default:
+                break;
+        }
+    }
+}
