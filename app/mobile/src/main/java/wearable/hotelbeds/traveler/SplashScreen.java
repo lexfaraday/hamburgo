80598dfe0962c6a145134ad196479664a1b9f3ec
lexfaraday <lexfaraday@outlook.com>
2015-10-12 17:25:05 +0200

    Add splash screen, move ic_launcher inside drawable

---
+++ app/mobile/src/main/java/wearable/hotelbeds/traveler/SplashScreen.java
@@ -0,0 +1,40 @@
+package wearable.hotelbeds.traveler;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by lexfaraday on 12/10/15.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
